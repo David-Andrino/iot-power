02172d2eb2a2d46fcde434380767259a18303189
SMBTime <126691426+SMBTime@users.noreply.github.com>
2024-11-22 18:19:05 +0100

    Envios Correctos
    Cambios para el envío al MQTT al topic correcto.

--- main/main.ino
+++ main/main.ino
@@ -1,94 +1,90 @@
+#include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <WiFiUdp.h>
-
-#define MSG_BUFFER_SIZE (50)
+#include <Wire.h>
+// Replace the next variables with your SSID/Password combination
 #define ssid "POC"
 #define password "cacacaca"
 
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.1.144";
+const char* mqtt_server = "192.168.238.69";
+
+char result[256];
+double VSolar = 30, ISolar = 18, VBatbu = 10, IBatbu = 22, VBat1 = 77, IBat1 = 88, VBat2 = 99, IBat2 = 123;
 
 WiFiClient wifiClient;
 WiFiUDP ntpUDP;
-PubSubClient mqttClient(wifiClient);
-
-const char* mqtt_server = "192.168.238.69"; // eg. your-demo.cedalo.cloud or 192.168.1.11
-const uint16_t mqtt_server_port = 4444; // or 8883 most common for tls transport
-const char* mqttTopicIn = "GGG3XocFtPy6XXWiOWDc";
-const char* mqttTopicOut = "esp-8266-out";
-double VSolar = 30, ISolar = 18, VBatbu = 10, IBatbu = 22, VBat1 = 77, IBat1 = 88, VBat2 = 99, IBat2 = 123;
-char result[256];
+PubSubClient client(wifiClient);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("InitCOM");
+
   setup_wifi();
-  mqttClient.setServer(mqtt_server, mqtt_server_port);
-  mqttClient.setCallback(callback);
+  client.setServer(mqtt_server, 4444);
+  client.setCallback(callback);
+
 
 }
-
-void loop() {
-  if (!mqttClient.connected()) {
-    //Si no se conecta se almacena. Internamente.
-    Serial.println("NO se conecta");
-    connect();
-  }else{
-    
-    //Serial.println("Se conecta");
-    //setup_wifi();
-    snprintf(result, sizeof(result), "VSolar=%.2f,ISolar=%.2f,VBatbu=%.2f,IBatbu=%.2f,VBat1=%.2f,IBat1=%.2f,VBat2=%.2f,IBat2=%.2f", VSolar, ISolar, VBatbu, IBatbu, VBat1, IBat1, VBat2, IBat2);
-    mqttClient.publish(mqttTopicOut,result);
-    //Almacenar todo el rato.
-    
-  }
-}
-
 
 void setup_wifi() {
   delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-#ifdef MQTT_TLS
-  wifiClient.setInsecure();
-#endif
+
+  Serial.println("");
   Serial.println("WiFi connected");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived on topic: '");
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
   Serial.print(topic);
-  Serial.print("' with payload: ");
-  for (unsigned int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
   }
   Serial.println();
 
-  String mensaje = "Conexión inicial realizada.";
-  mqttClient.publish(mqttTopicOut,(" ESP8266: " + mensaje).c_str());
 }
 
-
-void connect() {
-  while (!mqttClient.connected()) {
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    String mqttClientId = "tenant";
-    if (mqttClient.connect("Cliente","Cliente","Cliente")) {
+    // Attempt to connect
+    if (client.connect("Cliente","Cliente","Cliente")) {
       Serial.println("connected");
-      mqttClient.subscribe(mqttTopicIn);
+      // Subscribe
+      client.subscribe("v1/devices/me/telemetry");
     } else {
       Serial.print("failed, rc=");
-      Serial.print(mqttClient.state());
-      Serial.println(" will try again in 5 seconds");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
+}
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  snprintf(result, sizeof(result), "VSolar=%.2f,ISolar=%.2f,VBatbu=%.2f,IBatbu=%.2f,VBat1=%.2f,IBat1=%.2f,VBat2=%.2f,IBat2=%.2f", VSolar, ISolar, VBatbu, IBatbu, VBat1, IBat1, VBat2, IBat2);
+  client.publish("VSolar" ,result);
+
 }
