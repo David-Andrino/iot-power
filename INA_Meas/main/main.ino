86fea8bc6510e7d5b71a886664c7640774412f63
SMBTime <126691426+SMBTime@users.noreply.github.com>
2024-11-30 19:12:28 +0100

    Medidas_Bajo_Consumo
    Medidas a petición (cambiar el loop por un metodo cunado lo juntemos todo) y tras las medidas bajo consumo

--- INA_Meas/main/main.ino
+++ INA_Meas/main/main.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <INA226_WE.h>
+//Direcciones I2C
 #define I2C_D_PANEL  0x40
 #define I2C_D_BAT_BU 0x40
 #define I2C_D_BAT_1  0x40
@@ -13,79 +14,98 @@ float VSolar = 0, ISolar = 0, VBatbu = 0, IBatbu = 0, VBat1 = 0, IBat1 = 0, VBat
 
 void setup(){
   Serial.begin(9600);
-  while (!Serial); // wait until serial comes up on Arduino Leonardo or MKR WiFi 1010
+  while (!Serial);
   Wire.begin();
   Solar.init();
   Batbu.init();
   Bat1.init();
   Bat2.init();
+  //T_medida=Promediado_Numero_medidas*T_Conversion*2
 
-  /* Set conversion time in microseconds
-     One set of shunt and bus voltage conversion will take:
-     number of samples to be averaged x conversion time x 2
-
-       Mode *         * conversion time
+  /* Tiempo de conversión 
      CONV_TIME_140          140 µs
      CONV_TIME_204          204 µs
      CONV_TIME_332          332 µs
      CONV_TIME_588          588 µs
-     CONV_TIME_1100         1.1 ms (default)
+     CONV_TIME_1100         1.1 ms (Por defecto)
      CONV_TIME_2116       2.116 ms
      CONV_TIME_4156       4.156 ms
-     CONV_TIME_8244       8.244 ms  */
-
-  Solar.setConversionTime(CONV_TIME_1100); //choose conversion time and uncomment for change of default
+     CONV_TIME_8244       8.244 ms  
+  */
+  Solar.setConversionTime(CONV_TIME_1100);
   Batbu.setConversionTime(CONV_TIME_1100);
   Bat1.setConversionTime(CONV_TIME_1100);
   Bat2.setConversionTime(CONV_TIME_1100);
-  /* Set measure mode
-    POWER_DOWN - INA226 switched off
-    TRIGGERED  - measurement on demand
-    CONTINUOUS  - continuous measurements (default)*/
+  /*Modos de medida:
+    POWER_DOWN - Permite configurar el sistema en bajo consumo manteniendo la configuración
+               - El consumo se reduce de 0.35mA (Activo) a 2.3 uA (Bajo consumo) 
+    TRIGGERED  - Medidas a petición
+    CONTINUOUS - Medidas continuas
+  */
 
-  //ina226.setMeasureMode(CONTINUOUS); // choose mode and uncomment for change of default
+  Solar.setMeasureMode(POWER_DOWN);
+  Batbu.setMeasureMode(POWER_DOWN);
+  Bat1.setMeasureMode(POWER_DOWN);
+  Bat2.setMeasureMode(POWER_DOWN);
 
-  /* Set Resistor and Current Range
-     if resistor is 5.0 mOhm, current range is up to 10.0 A
-     default is 100 mOhm and about 1.3 A*/
+  /* Indicación de la resistencia se SHUNT y el rango de corrientes usados
+      .setResistorRange(R_SHUNT, I);
+  */
   
-  Solar.setResistorRange(0.1, 10); //Esto es para medir la corriente
+  Solar.setResistorRange(0.1, 10);//Cambiar resistencia si usamos otra, esta es para conexión directa
   Batbu.setResistorRange(0.1, 10);
   Bat1.setResistorRange(0.1, 10);
   Bat2.setResistorRange(0.1, 10);
   
   
-  /* If the current values delivered by the INA226 differ by a constant factor
-     from values obtained with calibrated equipment you can define a correction factor.
-     Correction factor = current delivered from calibrated equipment / current delivered by INA226*/
+  /* Si las medidas tienen un error debido al equipo por un factor constante se puede indicar:
+     Factor_Correccion = I_Equipo/I_INA
+  */
+
   /*
   Solar.setCorrectionFactor(0.95);
   Batbu.setCorrectionFactor(0.95);
   Bat1.setCorrectionFactor(0.95);
   Bat2.setCorrectionFactor(0.95);
   */
-  Solar.waitUntilConversionCompleted(); //if you comment this line the first data might be zero
+
+  //Comentar si estamos utilizando el modo de TRIGGERED
+  //Ya que se necesita esperar la conversión en los otros no hace falta.
+  
+  Solar.waitUntilConversionCompleted();
   Batbu.waitUntilConversionCompleted(); 
   Bat1.waitUntilConversionCompleted(); 
   Bat2.waitUntilConversionCompleted(); 
+  
 }
 
-void loop()
-{
+void loop(){
+
+  //Tomamos las medidas
+
   Solar.readAndClearFlags();
-  Batbu.readAndClearFlags();
-  Bat1.readAndClearFlags();
-  Bat2.readAndClearFlags();
-  //Nota el INA SOLO da valores V>0 Voltios
   VSolar = Solar.getBusVoltage_V()+(Solar.getShuntVoltage_mV()/100); 
   ISolar = Solar.getCurrent_mA(); 
+
+  Batbu.readAndClearFlags();
   VBatbu = Batbu.getBusVoltage_V()+(Batbu.getShuntVoltage_mV()/100); 
   IBatbu = Batbu.getCurrent_mA();
+    
+  Bat1.readAndClearFlags();
   VBat1  = Bat1.getBusVoltage_V()+(Bat1.getShuntVoltage_mV()/100);
   IBat1  = Bat1.getCurrent_mA(); 
+  
+  Bat2.readAndClearFlags();
   VBat2  = Bat2.getBusVoltage_V()+(Bat2.getShuntVoltage_mV()/100);
   IBat2  = Bat2.getCurrent_mA();
-  
+
+  //Ponemos los INA a bajo consumo
+
+  Solar.powerDown();
+  Batbu.powerDown(); 
+  Bat1.powerDown(); 
+  Bat2.powerDown();  
+
   Serial.print("VSolar [V]: "); Serial.println(VSolar);
   Serial.print("ISolar [mA]: "); Serial.println(ISolar);
   Serial.print("VBatbu [V]: "); Serial.println(VBatbu);
@@ -94,8 +114,22 @@ void loop()
   Serial.print("IBat1  [mA]: "); Serial.println(IBat1);
   Serial.print("VBat2  [V]: "); Serial.println(VBat2);
   Serial.print("IBat2  [mA]: "); Serial.println(IBat2);
-  
-  Serial.println();
 
-  delay(3000);
+  //Hacemos una espera de 5s y volvemos a tomar meidas:
+  Serial.println("Espera 5s");
+  for(int i=0; i<5; i++){
+    Serial.print(".");
+    delay(1000);
+  }
+  //Comentar para que al terminar las medidas quede a la espera hasta que queramos.
+  despertar_INAs();
+
+}
+
+void despertar_INAs(){
+  //Despertamos los INA de nuevo 
+  Solar.powerUp();
+  Batbu.powerUp(); 
+  Bat1.powerUp(); 
+  Bat2.powerUp();   
 }
