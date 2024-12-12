77ba9814aa02a5f4e270f800fc729c451278c5fc
SMBTime <126691426+SMBTime@users.noreply.github.com>
2024-12-12 09:53:59 +0100

    Añadidas las direcciones de los INA

--- MAIN_POWER/INA_medidas.h
+++ MAIN_POWER/INA_medidas.h
@@ -30,9 +30,9 @@
 #include <INA226_WE.h>
 #include "mqtt_client.hpp"
 // Direcciones I2C
-#define I2C_D_PANEL  0x41
-#define I2C_D_BAT_BU 0x41
-#define I2C_D_BAT_1  0x41
+#define I2C_D_PANEL  0x45
+#define I2C_D_BAT_BU 0x40
+#define I2C_D_BAT_1  0x44
 #define I2C_D_BAT_2  0x41
 
 
