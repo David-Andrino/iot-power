47c1889c67cd9f9372007c285c776f750678e03d
SMBTime <126691426+SMBTime@users.noreply.github.com>
2024-12-12 09:52:59 +0100

    Corrección error conexion MQTT

--- MAIN_POWER/mqtt_client.cpp
+++ MAIN_POWER/mqtt_client.cpp
@@ -2,7 +2,6 @@
 
 static void reconnect(PubSubClient& client, char* const mqtt_server, int mqtt_port);
 
-bool mqtt_conected=false;
 
 bool publishTelemetry(char* const mqtt_server, int mqtt_port, telemetry_t& telemetry){
     WiFiClient wifiClient;
@@ -13,19 +12,17 @@ bool publishTelemetry(char* const mqtt_server, int mqtt_port, telemetry_t& telem
     char result[256];
     if (!client.connected()){
       reconnect(client, mqtt_server, mqtt_port);
-    }else if (client.connected() || mqtt_conected==true){
-      mqtt_conected=false;
-      snprintf(result, sizeof(result), "{VSolar:%.2f,ISolar:%.2f,VBatbu:%.2f,IBatbu:%.2f,VBat1:%.2f,IBat1:%.2f,VBat2:%.2f,IBat2:%.2f}", 
-      telemetry.VSolar, telemetry.ISolar, telemetry.VBatbu, telemetry.IBatbu, telemetry.VBat1, telemetry.IBat1, telemetry.VBat2, telemetry.IBat2);
-      bool statusPublish = client.publish("v1/devices/me/telemetry" ,result);
-      client.disconnect();
     }
+    snprintf(result, sizeof(result), "{VSolar:%.2f,ISolar:%.2f,VBatbu:%.2f,IBatbu:%.2f,VBat1:%.2f,IBat1:%.2f,VBat2:%.2f,IBat2:%.2f}", 
+    telemetry.VSolar, telemetry.ISolar, telemetry.VBatbu, telemetry.IBatbu, telemetry.VBat1, telemetry.IBat1, telemetry.VBat2, telemetry.IBat2);
+    statusPublish = client.publish("v1/devices/me/telemetry" ,result);
+    client.disconnect();
     return statusPublish;
 }
 
 void reconnect(PubSubClient& client, char* const mqtt_server, int mqtt_port) {
   int j=0;
-  while (!client.connected() && j!=5) {
+  while (!client.connected() && j!=3) {
     Serial.print("Attempting MQTT connection to ");
     Serial.print(String(mqtt_server));
     Serial.print(":");
